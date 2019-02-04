47433378babf78b3a8bba63ee9f5f928d05443ee
Volker Böhme <volker.boehme@deesa.de>
2019-02-04 14:43:07 +0100

    Initital Commit

---
+++ docker-entrypoint.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+if [ `ls /etc/apache2/sites-available/ | wc -l` -eq 0 ]
+then
+  cp -r /etc/apache2/sites-available_default/* /etc/apache2/sites-available/
+fi
+
+if [ `ls /etc/letsencrypt/ | wc -l` -eq 0 ]
+then
+  cp -r /etc/letsencrypt_default/* /etc/letsencrypt/
+fi
+
+if [ `ls /var/www/html/upload/ | wc -l` -eq 0 ]
+then
+  cp -r /var/www/html/upload_default/* /var/www/html/upload/
+fi
+
+find /var/www/html/application/config ! -name 'config.php' -type f -exec rm -f {} +
+cp -r /var/www/html/application/config_default/* /var/www/html/application/config
+chown -R www-data:www-data /var/www/html
+chmod -R o-w-r-x /var/www/html/
+chmod -R -w /var/www/html
+chmod -R 770 /var/www/html/appliction/config
+chmod -R 770 /var/www/html/tmp
+chmod -R 770 /var/www/html/upload
+
+#List site and enable
+ls /etc/apache2/sites-available/ -1A | a2ensite *.conf
+
+#LETSECNRYPT
+if [ "$LETSENCRYPTDOMAINS" != "" ]
+then
+  /usr/sbin/apache2ctl start
+  certbot --apache -n -d $LETSENCRYPTDOMAINS --agree-tos --email $LETSENCRYPTEMAIL
+  /usr/sbin/apache2ctl stop
+fi
+
+#Start Cron
+/etc/init.d/anacron start
+/etc/init.d/cron start
+
+#Launch Apache on Foreground
+/usr/sbin/apache2ctl -D FOREGROUND
